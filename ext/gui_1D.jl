438203323c04029a59f91de49fce36abcfb2bafa
aris <aris.mav@hotmail.com>
2025-03-20 14:22:21 +0000

    reorganise gui extension, bump version

---
+++ ext/gui_1D.jl
@@ -0,0 +1,185 @@
+## Plots for 1D inversions
+
+"""
+    plot(res_mat::VecOrMat{inv_out_1D};kwargs...)
+
+Plot the results contained in a vector of `inv_out_1D` structures.
+
+The keyword (optional) arguments are:
+
+- `selections` : Whether to highlight the selections in the plots (default is `false`).
+
+"""
+function Makie.plot(res_mat::AbstractVecOrMat{NMRInversions.inv_out_1D}; selections = false)
+
+    fig = Figure(size=(700, 600))
+
+    res = res_mat[1]
+    # Make axes
+    if res.seq in [NMRInversions.IR]
+        ax1 = Axis(fig[1:5, 1:5], xlabel="time (s)", ylabel="Signal (a.u.)")
+        ax2 = Axis(fig[1:5, 6:10], xlabel="T₁ (s)", xscale=log10)
+        ax3 = Axis(fig[6:10,1:5], xlabel= "time (s)", ylabel="Residuals (a.u.)")
+
+    elseif res.seq in [NMRInversions.CPMG]
+        ax1 = Axis(fig[1:5, 1:5], xlabel="time (s)", ylabel="Signal (a.u.)")
+        ax2 = Axis(fig[1:5, 6:10], xlabel="T₂ (s)", xscale=log10)
+        ax3 = Axis(fig[6:10,1:5], xlabel= "time (s)", ylabel="Residuals (a.u.)")
+
+    elseif res.seq in [NMRInversions.PFG]
+        ax1 = Axis(fig[1:5, 1:5], xlabel="b factor (s/m² e-9)", ylabel="Signal (a.u.)")
+        ax2 = Axis(fig[1:5, 6:10], xlabel="D (m²/s)", xscale=log10)
+        ax3 = Axis(fig[6:10,1:5], xlabel= "b factor (s/m² e-9)", ylabel="Residuals (a.u.)")
+    end
+
+    linkxaxes!(ax1, ax3)
+
+    for r in res_mat
+        draw_on_axes(ax1, ax2, ax3, r, selections = selections)
+    end
+
+    return fig
+end
+
+
+"""
+    plot(res::inv_out_1D)
+
+Run the GUI to plot the 1D inversion results and select peaks you want to label.
+"""
+function Makie.plot(res::NMRInversions.inv_out_1D)
+
+    fig = plot([res], selections = true)
+
+    slider = IntervalSlider(fig[6,6:10], range = [1:length(res.X)...], startvalues = (1, length(res.X)))
+
+    int_low= lift(slider.interval) do i
+        return res.X[i[1]]
+    end
+
+    int_high= lift(slider.interval) do i
+        return res.X[i[2]]
+    end
+
+    weighted_average = lift(slider.interval) do i
+        return res.f[i[1]:i[2]]' * res.X[i[1]:i[2]] / sum(res.f[i[1]:i[2]]) 
+    end
+
+    labeltext = lift(slider.interval) do i
+        return "Selected range from $(round(res.X[i[1]], sigdigits=2)) to $(round(res.X[i[2]], sigdigits=2))
+        \n Weighted average = $(round(weighted_average[], sigdigits=3))"
+    end
+
+    Label(fig[7,6:10], labeltext, tellwidth=false)
+    vlines!(fig.content[2], int_low, color=:red)
+    vlines!(fig.content[2], int_high, color=:red)
+
+    button_label = Button(fig[8,6:10], label = "Label current selection")
+    button_filter = Button(fig[9,6:10], label = "Filter-out current selection")
+    button_reset = Button(fig[10,6:8], label = "Reset selections")
+    button_save = Button(fig[10,8:10], label = "Save and exit")
+
+    on(button_label.clicks) do _
+        push!(res.selections, slider.interval[])
+        empty!(fig.content[1])
+        empty!(fig.content[2])
+        empty!(fig.content[3])
+        draw_on_axes(fig.content[1], fig.content[2], fig.content[3], res, selections = true)
+        vlines!(fig.content[2], int_low, color=:red)
+        vlines!(fig.content[2], int_high, color=:red)
+    end
+
+    on(button_filter.clicks) do _
+        empty!(fig.content[1])
+        empty!(fig.content[2])
+        empty!(fig.content[3])
+        filter_range!(res, slider.interval[])
+        draw_on_axes(fig.content[1], fig.content[2], fig.content[3], res, selections = true)
+        vlines!(fig.content[2], int_low, color=:red)
+        vlines!(fig.content[2], int_high, color=:red)
+    end
+
+    on(button_reset.clicks) do _
+        empty!(res.selections)
+        res.filter = ones(length(res.X))
+        empty!(fig.content[1])
+        empty!(fig.content[2])
+        empty!(fig.content[3])
+        draw_on_axes(fig.content[1], fig.content[2], fig.content[3], res)
+        vlines!(fig.content[2], int_low, color=:red)
+        vlines!(fig.content[2], int_high, color=:red)
+    end
+
+    on(button_save.clicks) do _
+        savedir = NMRInversions.save_file(res.title, filterlist = "png")
+        f = plot([res], selections = true)
+        save(savedir, f)
+    end
+
+
+    return fig
+end
+
+
+function draw_on_axes(ax1, ax2, ax3, res::NMRInversions.inv_out_1D; selections = false)
+
+    # apply filter to f and update fitted curve and residuals
+    f_prime = res.filter .* res.f
+    yfit_prime = create_kernel(
+        res.seq, res.xfit, (res.seq == PFG ? res.X .* 1e9 : res.X)) * f_prime
+    r_prime = create_kernel(
+        res.seq, res.x, (res.seq == PFG ? res.X .* 1e9 : res.X)) * f_prime - res.y
+
+    c = length(ax2.scene.plots)
+    scatter!(ax1, res.x, real.(res.y), colormap=:tab10, colorrange=(1, 10), color=c)
+    lines!(ax1, res.xfit, yfit_prime, colormap=:tab10, colorrange=(1, 10), color=c)
+    lines!(ax2, res.X, f_prime, colormap=:tab10, colorrange=(1, 10), color=c)
+    lines!(ax3, res.x, r_prime, colormap=:tab10, colorrange=(1, 10), color=c)
+    scatter!(ax3, res.x, r_prime, colormap=:tab10, colorrange=(1, 10), color=c)
+
+
+    if selections
+
+        if res.seq in [NMRInversions.IR]
+            Xlabel = ["<T₁> = ", " (s)"]
+        elseif res.seq in [NMRInversions.CPMG]
+            Xlabel = ["<T₂> = ", " (s)"]
+        elseif res.seq in [NMRInversions.PFG]
+            Xlabel = ["<D> = ", " (m²/s)"]
+        end
+
+        wa = weighted_averages(res)
+
+        for (i,s) in enumerate(res.selections)
+            clr = only(
+                Makie.numbers_to_colors(
+                    [i], 
+                    Makie.to_colormap(:tab10), 
+                    identity, 
+                    Vec2(1, 10), 
+                    Makie.automatic, 
+                    Makie.automatic, 
+                    RGBAf(0, 0, 0, 0)
+                )
+            )
+
+            band!(
+                ax2, 
+                res.X[s[1]:s[2]],
+                zeros(length(res.f[s[1]:s[2]])),
+                (res.f .* res.filter)[s[1]:s[2]],
+                color = clr, alpha = 0.5
+            )
+
+            height = (1 - (i-1) * 0.1) * maximum(res.f .* res.filter) 
+            text!(
+                ax2, res.X[2], height, 
+                text = Xlabel[1]*"$(round(wa[i], sigdigits = 2))"*Xlabel[2], 
+                align = (:left, :top), color = clr
+            )
+
+            #=vlines!(ax2, res.X[s[1]], color = clr, alpha = 0.8)=#
+            #=vlines!(ax2, res.X[s[2]],color = clr, alpha = 0.8)=#
+        end
+    end
+end
---
+++ ext/gui_expfits.jl
@@ -0,0 +1,76 @@
+## Plots for expfits
+"""
+    plot(res::expfit_struct...; kwargs...)
+Plot the results of an `expfit` call. \n
+
+This function can take any number of `expfit_struct` structures as input. \n
+e.g. `plot(data1)` plots the data and the corresponding fit,
+but `plot(data1, data2)` or `plot(data1, data2, data3)` work as well,
+and they plot all of the results on the same plot.
+
+Keyword (optional) arguments:
+- `markersize` : The size of the markers (default is 7).
+- `normeq` : Whether to plot the normalised form of the equation or not (default is `true`).
+- `yscale` : The scale of the y-axis (default is `identity`).
+
+"""
+function Makie.plot(res::expfit_struct...; kwargs...)
+
+    f = Figure(size=(800, 400))
+    plot!(f, res...; kwargs...)
+
+    return f
+
+end
+
+function Makie.plot(res_mat::AbstractVecOrMat{NMRInversions.expfit_struct}; kwargs...)
+
+    f = Figure(size=(800, 400))
+    plot!(f, res_mat...; kwargs...)
+
+    return f
+
+end
+
+"""
+    plot!(fig, res...; names, markersize, normeq)
+
+Plots the results of an `expfit` call on a figure or a grid position object.
+
+The arguments are:
+
+- `fig` : The figure or grid position object.
+- `res` : One or more `expfit_struct` structures containing the fit results.
+
+Keyword (optional) arguments:
+- `markersize` : The size of the markers (default is 7).
+- `normeq` : Whether to plot the normalised form of the equation or not (default is `true`).
+- `yscale` : The scale of the y-axis (default is `identity`).
+
+Note that the res inputs are not a vector, but individual `expfit_struct` structures,
+like: plot!(fig, data1, data2, data3).
+If you want to use a vector of `expfit_struct` structures, make sure to 
+splat it by using `...` in the function call (e.g. `plot!(fig, [data1, data2, data3]...)`).
+"""
+function Makie.plot!(fig::Union{Makie.Figure,Makie.GridPosition}, res::expfit_struct...;
+     markersize=7, normeq=true, yscale = identity)
+
+    # Make axes
+    if res[1].seq in [NMRInversions.IR]
+        ax = Axis(fig[1, 1], xlabel="time (s)", ylabel="Signal (a.u.)",yscale=yscale)
+
+    elseif res[1].seq in [NMRInversions.CPMG]
+        ax = Axis(fig[1, 1], xlabel="time (s)", ylabel="Signal (a.u.)",yscale=yscale)
+
+    elseif res[1].seq in [NMRInversions.PFG]
+        ax = Axis(fig[1, 1], xlabel="b factor (s/m² e-9)", ylabel="Signal (a.u.)",yscale=yscale)
+    end
+
+    for r in res
+        scatter!(ax, r.x, r.y, markersize=markersize, label= r.title )
+        lines!(ax, r.xfit, r.yfit, label=getfield(r, normeq == true ? :eqn : :eq))
+    end
+
+    axislegend(ax, position=(res[1].seq == IR ? :rb : :rt), nbanks=2)
+
+end
