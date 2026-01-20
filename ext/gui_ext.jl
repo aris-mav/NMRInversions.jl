1553abab1848ad658349ea97cb21ae2c9d121ff3
arismav <aris.mav@hotmail.com>
2026-01-20 22:05:51 +0100

    non-interactive plots working okay

--- ext/gui_ext.jl
+++ ext/gui_ext.jl
@@ -5,7 +5,6 @@ using NMRInversions, GLMakie, PolygonOps, LinearAlgebra, NativeFileDialog
 include("./gui_1D.jl")
 include("./gui_2D.jl")
 include("./gui_expfits.jl")
+include("./gui_data.jl")
 
 end
-
-
