14b0f65271fc600b6f77736fe6ce660262e9006a
Aristarchos Mavridis <116081909+aris-mav@users.noreply.github.com>
2026-01-21 13:52:10 +0100

    Plot raw data (#80)
    * non-interactive plots working okay
    
    * adding file
    
    * sliders working okay
    
    * show point where the dir and indir meet
    
    * add large data warning
    
    * add docstrings
    
    * update docs
    
    ---------
    
    Co-authored-by: arismav <aris.mav@hotmail.com>

---
+++ ext/gui_data.jl
@@ -0,0 +1,98 @@
+"""
+plot(data::input1D)
+
+Quickly visualise the contents of a 1D NMR data structure.
+"""
+function Makie.plot(data::NMRInversions.input1D)
+    fig = Figure()
+    ax = Axis(fig[:,:])
+
+    if any(isa.(data.y, Complex))
+        plot!(ax,data.x,imag.(data.y),color = Cycled(2), label= "Complex part")
+        plot!(ax,data.x,real.(data.y),color = Cycled(1), label= "Real part")
+        axislegend(ax)
+    else
+        plot!(ax,data.x,data.y)
+    end
+
+    ax.ylabel = "Signal (a.u.)"
+    if data.seq in (IR, SR)
+        ax.xlabel = "time (s)"
+    elseif data.seq == CPMG
+        ax.xlabel = "time (s)"
+    elseif data.seq == PFG
+        ax.xlabel = "b factor (s/m² e-9)"
+    end
+
+    return fig
+end
+
+
+"""
+plot(data::input2D)
+
+Interactive GUI to visualise the contents of a 2D NMR data structure.
+"""
+function Makie.plot(data::NMRInversions.input2D)
+
+    dt = real.(data.data)
+    xd = data.x_direct
+    xi = data.x_indirect
+
+    if any(map( x -> length(x) > 16000 , [xd,xi]))
+
+        dim = length(xd) > length(xi) ? "direct" : "indirect"
+        excess = maximum(map( x -> length(x), [xd,xi])) - 16000
+
+        throw("Dataset too big to plot, \
+              Makie throws errors. \
+              Consider using `trim` to have \
+              fewer than 16000 points on either \
+              dimensions. e.g. \
+              'plot(trim(data, $dim=(0,$excess)))` \
+              should work, as it will remove $excess \
+              points from the end of the $dim dimension data"
+              )
+    end
+
+    fig = Figure()
+    ax_dir = Axis(fig[1:4,1:5], ylabel= "Signal (a.u.)", 
+                  title= "Direct dimension")
+    ax_indir = Axis(fig[6:9,1:5], ylabel= "Signal (a.u.)",
+                    title = "Indirect dimension")
+    ax_full = Axis3(fig[1:9,6:10], zticklabelsize = 0.1, zlabelvisible=false)
+
+    sld = Slider(fig[5, 1:5], range = 1:length(xi), startvalue = 1)
+    sli = Slider(fig[10, 1:5], range = 1:length(xd), startvalue = 1)
+
+    indir_slice = @lift(dt[$(sli.value),:])
+    dir_slice = @lift(dt[:,$(sld.value)])
+
+    scatter!(ax_dir, xd, dir_slice, color = Cycled(1))
+    scatter!(ax_indir, xi, indir_slice, color = Cycled(2))
+    surface!(ax_full, xd, xi, dt, alpha=0.5)
+    scatter!(ax_full, xd , @lift(fill(xi[$(sld.value)] ,length(xd)) ), dir_slice, color = Cycled(1), markersize = 10)
+    scatter!(ax_full, @lift(fill(xd[$(sli.value)] ,length(xi)) ), xi , indir_slice, color = Cycled(2), markersize = 10)
+
+    scatter!(ax_dir, @lift(xd[$(sli.value)]), @lift(dt[$(sli.value),$(sld.value)]), color = Cycled(2))
+    scatter!(ax_indir, @lift(xi[$(sld.value)]), @lift(dt[$(sli.value),$(sld.value)]), color = Cycled(1))
+
+    y_low = minimum(dt) * 1.1
+    y_high = maximum(dt) * 1.1
+    ax_dir.limits = (nothing,nothing,y_low,y_high)
+    ax_indir.limits = (nothing,nothing,y_low,y_high)
+
+    ax_indir.ylabel = "Signal (a.u.)"
+    ax_dir.xlabel = "time (s)"
+    ax_full.xlabel = "time (s)"
+
+    if data.seq == IRCPMG
+        ax_indir.xlabel = "time (s)"
+        ax_full.ylabel = "time (s)"
+    elseif data.seq == PFGCPMG
+        ax_indir.xlabel = "b factor (s/m² e-9)"
+        ax_full.ylabel = "b factor (s/m² e-9)"
+    end
+
+    return fig
+end
