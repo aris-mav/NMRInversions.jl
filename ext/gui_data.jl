6a9b65929931387dff5ca08a23f763262fce9114
arismav <aris.mav@hotmail.com>
2026-01-21 11:27:40 +0100

    sliders working okay

--- ext/gui_data.jl
+++ ext/gui_data.jl
@@ -42,12 +42,31 @@ function Makie.plot(data::NMRInversions.input2D)
                     title = "Indirect dimension")
     ax_full = Axis3(fig[1:9,6:10], zticklabelsize = 0.1, zlabelvisible=false)
 
-    surface!(ax_full,data.x_direct, data.x_indirect, real.(data.data))
-    scatter!(ax_dir, data.x_direct, real.(data.data)[:,1],color = Cycled(1))
-    scatter!(ax_indir, data.x_indirect, real.(data.data)[1,:],color = Cycled(2))
+    d = real.(data.data)
+    xd = data.x_direct
+    xi = data.x_indirect
 
+    sld = Slider(fig[5, 1:5], range = 1:length(xi), startvalue = 1)
+    sli = Slider(fig[10, 1:5], range = 1:length(xd), startvalue = 1)
+
+    indir_slice = @lift(d[$(sli.value),:])
+    dir_slice = @lift(d[:,$(sld.value)])
+
+    scatter!(ax_dir, xd, dir_slice, color = Cycled(1))
+    scatter!(ax_indir, xi, indir_slice, color = Cycled(2))
+    surface!(ax_full,xd, xi, d, alpha=0.5)
+    scatter!(ax_full, xd , @lift(fill(xi[$(sld.value)] ,length(xd)) ), dir_slice, color = Cycled(1), markersize = 10)
+    scatter!(ax_full, @lift(fill(xd[$(sli.value)] ,length(xi)) ), xi , indir_slice, color = Cycled(2), markersize = 10)
+
+    y_low = minimum(d) * 1.1
+    y_high = maximum(d) * 1.1
+    ax_dir.limits = (nothing,nothing,y_low,y_high)
+    ax_indir.limits = (nothing,nothing,y_low,y_high)
+
+    ax_indir.ylabel = "Signal (a.u.)"
     ax_dir.xlabel = "time (s)"
     ax_full.xlabel = "time (s)"
+
     if data.seq == IRCPMG
         ax_indir.xlabel = "time (s)"
         ax_full.ylabel = "time (s)"
