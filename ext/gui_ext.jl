4fffb06d6ae24851746b42ba45fd613e5221a42d
arismav <aris.mav@hotmail.com>
2026-06-19 14:59:52 +0200

    data plots work

--- ext/gui_ext.jl
+++ ext/gui_ext.jl
@@ -2,9 +2,27 @@ module gui_ext
 
 using NMRInversions, GLMakie, PolygonOps, LinearAlgebra, NativeFileDialog
 
-include("./gui_1D.jl")
-include("./gui_2D.jl")
-include("./gui_expfits.jl")
+function x_label(ax::DataAxis)
+    if ax isa Union{IR, SR, CPMG}
+        return "time (s)"
+    elseif ax isa PFG
+        return "b factor (s/m² e-9)"
+    end
+end
+
+function X_label(ax::DataAxis)
+    if ax isa Union{IR, SR}
+        return L"T_1 \, \textrm{(s)}"
+    elseif ax isa CPMG
+        return L"T_2 \, \textrm{(s)}"
+    elseif ax isa PFG
+        return "D (m²/s)"
+    end
+end
+
 include("./gui_data.jl")
+# include("./gui_1D.jl")
+# include("./gui_2D.jl")
+# include("./gui_expfits.jl")
 
 end
