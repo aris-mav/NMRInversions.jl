c3843e63d1e37844f946e5b12fdb0c3a68aa0522
arismav <aris.mav@hotmail.com>
2026-07-27 15:36:08 +0200

    fix docstring newlines

--- src/invert.jl
+++ src/invert.jl
@@ -6,16 +6,16 @@ Receives `ExperimentData`, returns `InversionData`.
 
 The keyword arguments are:
 
-- `axes` : a D-dimensional tuple where each element is an array containing the 
-range of the output (e.g. relaxation times or diffusion coefficients) in 
-every corresponding dimension. `nothing` can be passed instead of an array in 
-any of the tuple elements, in which case a sensible array will be generated 
+- `axes` : a D-dimensional tuple where each element is an array containing the \
+range of the output (e.g. relaxation times or diffusion coefficients) in \
+every corresponding dimension. `nothing` can be passed instead of an array in \
+any of the tuple elements, in which case a sensible array will be generated \
 automatically (that is also the default option).
 - `alpha` : can either be a single scalar value (e.g. 0.65) or an `AlphaOptimizer`.
 - `solver` : is the `RegularizationSolver` used to solve the optimization problem.
-- `silent` : whether the function should be printing information while it runs 
+- `silent` : whether the function should be printing information while it runs \
 (defaults to `false`).
-- `scale` : whether the input data should be scaled to a maximum value of 1 
+- `scale` : whether the input data should be scaled to a maximum value of 1 \
 (you may also call that "normalizing" the data).
 """
 function invert(
